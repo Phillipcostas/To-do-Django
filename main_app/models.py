a9f5bd649c0aab817684a46686e268f15a4c5340
PhillipCostas <Phillipcostas@gmail.com>
2024-07-29 09:18:13 -0400

    fixed something, still not fully working correctly

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Todo(models.Model):
     name = models.CharField(max_length=150)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
 
     
     def __str__(self):
@@ -15,6 +15,6 @@ class Todo(models.Model):
         return reverse("toDo-detail", kwargs={"toDo_id": self.id})
 
 class SubTask(models.Model):
-    todo = models.ForeignKey(Todo, related_name='subtasks', on_delete=models.CASCADE)
+    todo = models.ForeignKey(Todo, related_name='subtasks', on_delete=models.CASCADE,)
     task = models.CharField(max_length=150)
     completed = models.BooleanField(default=False)
